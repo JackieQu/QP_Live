74f6ff531248925cd7e9081803a24703d277d2f0
JackieQu <13263699826@163.com>
2017-03-07 23:27:30 +0800

    Initial Commit

---
+++ QP_Live/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QP_Live
+//
+//  Created by JackieQu on 2017/3/7.
+//  Copyright © 2017年 JackieQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QP_Live/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QP_Live
+//
+//  Created by JackieQu on 2017/3/7.
+//  Copyright © 2017年 JackieQu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
